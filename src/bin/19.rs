299afd5ac559b3fe84125e461b9eebc16a72b778
Tyrone Wu <wudevelops@gmail.com>
2024-12-19 19:33:28 +0000

    day 19 without cached crate hack

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use cached::proc_macro::cached;
 
 advent_of_code::solution!(19);
@@ -23,14 +25,12 @@ fn dfs(available_patterns: &[String], target: &str) -> bool {
     if target.is_empty() {
         return true;
     }
-    for pat in available_patterns.iter() {
-        if let Some(design) = target.strip_suffix(pat) {
-            if dfs(available_patterns, design) {
-                return true;
-            }
-        }
-    }
-    false
+    available_patterns.iter().any(|pat| {
+        target
+            .strip_suffix(pat)
+            .map(|design| dfs(available_patterns, design))
+            .unwrap_or(false)
+    })
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -48,27 +48,59 @@ fn dfs_p2(available_patterns: Vec<String>, target: String) -> u64 {
     if target.is_empty() {
         return 1;
     }
-    let mut arrangements = 0;
-    for pat in available_patterns.iter() {
-        if let Some(design) = target.strip_suffix(pat) {
-            arrangements += dfs_p2(available_patterns.clone(), design.to_owned());
-        }
+    available_patterns
+        .iter()
+        .filter_map(|pat| {
+            target
+                .strip_suffix(pat)
+                .map(|design| dfs_p2(available_patterns.clone(), design.to_owned()))
+        })
+        .sum()
+}
+
+fn dfs_no_macro(
+    available_patterns: &[String],
+    target: &str,
+    cache: &mut HashMap<String, usize>,
+) -> usize {
+    if target.is_empty() {
+        return 1;
     }
+    if let Some(arrangements) = cache.get(target) {
+        return *arrangements;
+    }
+    let arrangements = available_patterns
+        .iter()
+        .map(|pat| {
+            target
+                .strip_prefix(pat)
+                .map(|design| dfs_no_macro(available_patterns, design, cache))
+                .unwrap_or_default()
+        })
+        .sum();
+    cache.insert(target.to_owned(), arrangements);
     arrangements
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(input: &str) -> Option<usize> {
     let (available_patterns, designs) = parse_input(input);
     let filtered_designs: Vec<String> = designs
         .into_iter()
         .filter(|d| dfs(&available_patterns, d))
         .collect();
+    let mut cache = HashMap::new();
     Some(
         filtered_designs
-            .into_iter()
-            .map(|d| dfs_p2(available_patterns.clone(), d))
+            .iter()
+            .map(|d| dfs_no_macro(&available_patterns, d, &mut cache))
             .sum(),
     )
+    // Some(
+    //     filtered_designs
+    //         .into_iter()
+    //         .map(|d| dfs_p2(available_patterns.clone(), d))
+    //         .sum(),
+    // )
 }
 
 #[cfg(test)]
